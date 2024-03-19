d8c5de09b18dad46c234d8763a48826744e798bd
Brian <mulindijrn@gmail.com>
2024-03-19 23:31:31 +0300

    Add hero section with welcome message and call-to-action button.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,13 @@ import './Home.css'
 function Home() {
   return (
     <>
-      
+      <div className='hero-bg'>
+        <div className='hero-content'>
+          <h1>WELCOME TO MEDILAB</h1>
+          <p>Medilab: Where Compassionate Care Meets Cutting-Edge Medicine</p>
+          <NavLink to="/about"><button>Get Started</button></NavLink> 
+        </div>
+      </div>
     </>
   )
 }
