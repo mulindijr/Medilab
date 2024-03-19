d240b6ee487162c6beed016946d62e6d4f0898bf
Brian <mulindijrn@gmail.com>
2024-03-19 23:29:49 +0300

    Add Home component structure and import necessary dependencies.

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { MdHealthAndSafety } from "react-icons/md";
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Home
