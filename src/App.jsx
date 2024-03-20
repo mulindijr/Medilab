8d7bbc45bd30b479b38531419b5da397ad75a16f
Brian <mulindijrn@gmail.com>
2024-03-21 00:54:17 +0300

    Set up routing for app and integrate Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+
 
 function App() {
   
   return (
     <>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<Home />}/>
+          <Route path='/services' element={<Home />}/>
+          <Route path='/departments' element={<Home />}/>
+          <Route path='/doctors' element={<Home />}/>
+          <Route path='/contact' element={<Home />}/>
+          <Route path='/appointment' element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
       
     </>
   )
