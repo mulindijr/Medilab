caf18aa74884ce5e9f6946914aa5398aa360bf97
Brian <mulindijrn@gmail.com>
2024-03-21 00:33:09 +0300

    Add hero section content and cards to homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,6 +13,28 @@ function Home() {
           <p>Medilab: Where Compassionate Care Meets Cutting-Edge Medicine</p>
           <NavLink to="/about"><button>Get Started</button></NavLink> 
         </div>
+        <div className='hero-cards-container'>
+          <div className='why-medilab'>
+            <h1>Why Choose Medilab?</h1>
+            <p>With a rich legacy of medical expertise spanning decades, we provide comprehensive, patient-centered care tailored to your individual needs. Our multidisciplinary team of dedicated healthcare professionals delivers compassionate support and cutting-edge treatments in our state-of-the-art facilities. We prioritize your well-being above all else, ensuring that you receive the highest quality care in a safe and comfortable environment.</p>
+            <NavLink to="/"><button>Learn More<MdKeyboardArrowRight className='arrow-right' /></button></NavLink>
+          </div>
+          <div className='hero-cards'>
+            <div className='hero-cards-icons'><MdHealthAndSafety /></div>
+            <h3>Mission</h3>
+            <p>At Medilab, our mission is to provide accessible, high-quality healthcare services to our community. We are committed to delivering compassionate care, utilizing innovative technologies, and fostering a culture of continuous improvement.</p>
+          </div>
+          <div className='hero-cards'>
+            <div className='hero-cards-icons'><MdHealthAndSafety /></div>
+            <h3>Vision</h3>
+            <p>Our vision at Medilab is to be a leader in healthcare excellence, recognized for our unwavering commitment to patient-centered care, clinical innovation, and community engagement.</p>
+          </div>
+          <div className='hero-cards'>
+            <div className='hero-cards-icons'><MdHealthAndSafety /></div>
+            <h3>Motto</h3>
+            <p>Care Beyond Measure</p>
+          </div>
+        </div>
       </div>
     </>
   )
