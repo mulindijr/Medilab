0d4d6019242eba101a7197e07888e3a207c16237
Brian <mulindijrn@gmail.com>
2024-03-22 01:31:42 +0300

    Implement active section tracking in Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { FaTwitter, FaInstagram, FaLinkedin, FaFacebook } from 'react-icons/fa';
 import { TfiEmail } from "react-icons/tfi";
 import { BsPhone } from "react-icons/bs";
-import { NavLink } from 'react-router-dom';
-import './Navbar.css'
+import { Link } from 'react-scroll';
+import './Navbar.css';
 
 function Navbar() {
+  const [activeSection, setActiveSection] = useState('');
+
+  const handleSetActive = (to) => {
+    setActiveSection(to);
+  };
   return (
     <>
       <div className='topbar'>
         <div className='topbar-left'>
-          <p  className='mail-to'>
+          <p className='mail-to'>
             <a href="mailto:mulindijrn@gmail.com">
               <TfiEmail /> contact@medilab.com
             </a>
@@ -33,21 +38,10 @@ function Navbar() {
         </div>
       </div>
       <div className='nav-bar'>
-        <nav>
-          <h3><NavLink to="/">MEDILAB</NavLink></h3>
-          <ul className='navbar'>
-          <li><NavLink exact='true' to="/" activeClassName="active">Home</NavLink></li>
-            <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
-            <li><NavLink to="/services" activeClassName="active">Services</NavLink></li>
-            <li><NavLink to="/departments" activeClassName="active">Departments</NavLink></li>
-            <li><NavLink to="/doctors" activeClassName="active">Doctors</NavLink></li>
-            <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
-            <NavLink to="/appointment" activeClassName="active"><button>Make an Appointment</button></NavLink>
-          </ul>
-        </nav>
+      
       </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
