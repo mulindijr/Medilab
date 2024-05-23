48d1c0fdc205f84f60bcf74ecab75f9df86f1030
Brian <mulindijrn@gmail.com>
2024-05-23 12:57:04 +0300

    Add initial structure for Count component with necessary imports
    Create the Count component with a div element.
    Import React, required icons from react-icons, CountUp for animated counters, and CSS for styling.
    Use the useInView hook from react-intersection-observer for future implementation.
    Add a ref to the main div for intersection observer functionality.

---
+++ src/components/Counts/Count.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FaFlask, FaAward } from "react-icons/fa";
+import { FaRegHospital, FaUserDoctor } from "react-icons/fa6";
+import './Count.css';
+import { useInView } from 'react-intersection-observer';
+import CountUp from 'react-countup';
+
+function Count() {
+  
+
+  return (
+    <div className='count' ref={ref}>
+           
+    </div>
+  );
+}
+
+export default Count;
