036cb3772a548ca045f2fe9a2088c41554427832
Brian <mulindijrn@gmail.com>
2024-05-23 12:49:01 +0300

    Add detailed content to About component
    Introduce a div with the className 'about-right' to contain text content.
    Add heading and introductory paragraph about Medilab.
    Create multiple 'about-content' sections, each containing an icon and corresponding text.
    Use RiTeamFill, HiMiniFingerPrint, and FaPhoneAlt icons to visually represent different content sections.
    Provide detailed descriptions for 'Our Team', 'Our Facilities', and 'Get in Touch' sections.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,7 +11,40 @@ function About() {
       <div className='about-left'>
           <img src={doctors} alt="doctors" />
       </div>
-      
+      <div className='about-right'>
+        <h3>About Medilab</h3>
+        <p>At Medilab, we are committed to providing exceptional healthcare services tailored to meet the diverse needs of our patients. With a legacy of excellence spanning decades, we have established ourselves as a trusted name in the healthcare industry.</p>
+        <div className='about-content'>
+          <div className='icon-box'>
+            <RiTeamFill className='about-icon'/>
+          </div>
+          <div>
+            <h4>Our Team</h4>
+            <p>At Medilab, our team consists of highly skilled and compassionate healthcare professionals who are dedicated to providing the best possible care to our patients. From our experienced physicians to our caring support staff, everyone at Medilab plays a vital role in our commitment to excellence.</p>
+          </div>
+          
+        </div>
+        <div className='about-content'>
+          <div className='icon-box'>
+            <HiMiniFingerPrint className='about-icon'/>
+          </div>
+          
+          <div>
+            <h4>Our Facilities</h4>
+            <p>We take pride in our state-of-the-art facilities, equipped with the latest technology and designedto ensure patient comfort and safety. From advanced diagnostic tools to modern treatment options, our facilities are equipped to meet the diverse needs of our patients.</p>
+          </div>
+          
+        </div>
+        <div className='about-content'>
+          <div className='icon-box'>
+            <FaPhoneAlt className='about-icon'/>
+          </div>
+          <div>
+            <h4>Get in Touch</h4>
+            <p>For more information about our services or to schedule an appointment, please contact us today. We look forward to serving you and your family with the highest standard of care.</p>
+          </div>
+        </div>
+      </div>
     </section>    
   )
 }
