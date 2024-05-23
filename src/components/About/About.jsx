1d344e45eb10216d4ca6771aa39895f92218ce82
Brian <mulindijrn@gmail.com>
2024-05-23 12:34:43 +0300

    Added initial structure for About component with necessary imports
    Created the About component with a section element.
    Imported relevant assets and icons for future use.
    Applied basic CSS import for styling.

---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import doctors from '../../assets/about.jpg'
+import { RiTeamFill } from "react-icons/ri";
+import { HiMiniFingerPrint } from "react-icons/hi2";
+import { FaPhoneAlt } from "react-icons/fa";
+import './About.css'
+
+function About() {
+  return (
+    <section id='about'>
+      
+    </section>    
+  )
+}
+
+export default About
