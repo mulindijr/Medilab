3ad6debefc5bfb2808e1e367fbcfa1ff1ac4dbbd
Brian <mulindijrn@gmail.com>
2024-05-23 14:41:39 +0300

    Add components to App
    Import necessary components: Navbar, Home, About, and Count.
    Integrate components into the App inside BrowserRouter.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import About from './components/About/About'
+import Count from './components/Counts/Count'
 
 
 function App() {
@@ -10,9 +11,10 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar/>
-        <Home/>
-        <About/>
+        <Navbar />
+        <Home />
+        <About />
+        <Count />
         
       </BrowserRouter>
       
