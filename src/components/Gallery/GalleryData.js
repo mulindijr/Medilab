579879081f0668a1b3d2db41854b714e3378a79c
Brian <mulindijrn@gmail.com>
2024-07-20 00:59:59 +0300

    Add the gallery data that is the images  with their respective id

---
+++ src/components/Gallery/GalleryData.js
@@ -0,0 +1,21 @@
+import galleryImage1 from '../../assets/gallery/gallery-1.jpg';
+import galleryImage2 from '../../assets/gallery/gallery-2.jpg';
+import galleryImage3 from '../../assets/gallery/gallery-3.jpg';
+import galleryImage4 from '../../assets/gallery/gallery-4.jpg';
+import galleryImage5 from '../../assets/gallery/gallery-5.jpg';
+import galleryImage6 from '../../assets/gallery/gallery-6.jpg';
+import galleryImage7 from '../../assets/gallery/gallery-7.jpg';
+import galleryImage8 from '../../assets/gallery/gallery-8.jpg';
+
+const galleryData = [
+  { id: 1, src: galleryImage1, alt: 'Gallery Image 1' },
+  { id: 2, src: galleryImage2, alt: 'Gallery Image 2' },
+  { id: 3, src: galleryImage3, alt: 'Gallery Image 3' },
+  { id: 4, src: galleryImage4, alt: 'Gallery Image 4' },
+  { id: 5, src: galleryImage5, alt: 'Gallery Image 5' },
+  { id: 6, src: galleryImage6, alt: 'Gallery Image 6' },
+  { id: 7, src: galleryImage7, alt: 'Gallery Image 7' },
+  { id: 8, src: galleryImage8, alt: 'Gallery Image 8' }
+];
+
+export default galleryData;
